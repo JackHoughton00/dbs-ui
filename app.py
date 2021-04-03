36190c7ab41761fb76001e4cf8e8be68b5e53792
Hamid-0 <qzz6Pn2AmPTQD9E>
2021-04-03 18:37:58 -0400

    LESSSS GOOOOOOOOOOOOOOOOOOO

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import random
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.automap import automap_base
 import datetime
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 
@@ -12,8 +13,8 @@ if ENV == 'dev':
     # In the next line change USERNAME to your uOttawa login before the @uOttawa.ca and change PASSWORD to your
     # uOttawa password
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://USER:PASS@web0.eecs.uottawa.ca:15432/group_a03_g30'
-       
+        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://habol085:rb7FkRCtMqJ5Ved@web0.eecs.uottawa.ca:15432/group_a03_g30'
+
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
@@ -26,77 +27,78 @@ rooms = db.Table('room', db.metadata, autoload=True, autoload_with=db.engine, sc
 
 rooms_hotel_chain = db.Table('room_hotel_chain', db.metadata, autoload=True, autoload_with=db.engine, schema='hoteldbs')
 
+hotel_chain = db.Table('hotel_chain', db.metadata, autoload=True, autoload_with=db.engine, schema='hoteldbs')
 
-hotel_chain = db.Table('hotel_chain',db.metadata,autoload = True, autoload_with=db.engine, schema = 'hoteldbs')
-
-
-# Base = automap_base()
-# Base.prepare(db.engine,  reflect =True, schema = 'hoteldbs')
-# #Rooms = Base.classes.rooms
-# #RoomHotelChain = Base.classes.room_hotel_chain
-# RoomHC = Base.classes.room_hotel_chain 
-
- 
-# class Room_hotel_chain(db.Model):
-#     __tablename__ = 'room_hotel_chain'
-#     __table_args__ = {'schema': 'hoteldbs'}
-
-#     room_number = db.Column(db.Integer, primary_key = True)
-#     hotel_id = db.Column(db.Integer)
-
-#     def __init__(self, room_number,hotel_id):
-#         self.room_number = room_number
-#         self.hotel_id = hotel_id
+Base = declarative_base()
 
 
+class Rooms(Base):
+    __tablename__ = 'room'
+    __table_args__ = {'schema': 'hoteldbs', 'extend_existing': True}
+    room_number = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.NUMERIC)
+    date_available = db.Column(db.DateTime)
+    room_capacity = db.Column(db.VARCHAR(50))
+    view_type = db.Column(db.VARCHAR(50))
+    amenities = db.Column(db.VARCHAR(50))
+    extendable = db.Column(db.BOOLEAN)
 
 
-
-# class Rooms(Base):
-#     __tablename__ = 'room'
-#     __table_args__ = {'schema': 'hoteldbs'}
-
-#     room_number = db.Column(db.Integer, primary_key = True)
-#     price = db.Column(db.Double)
-#     date_available 
+class Booking_Room(Base):
+    __tablename__ = 'booking_room'
+    __table_args__ = {'schema': 'hoteldbs', 'extend_existing': True}
+    booking_number = db.Column(db.Integer, primary_key=True)
+    room_number = db.Column(db.Integer, primary_key=True)
 
 
-
-# class EmpTable(Base):
-#     __tablename__ = 'employee'
-#     #below line of code will be how you direct the table to the right schema in the database on postrgre.
-#     __table_args__ = {'schema':'hoteldbs'}
-#     emp_sin_number = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(50))
-#     middle_name = db.Column(db.String(50))
-#     last_name = db.Column(db.String(50))
-#     house_number = db.Column(db.String(50))
-#     street_name = db.Column(db.String(50))
-#     postal_code = db.Column(db.String(50))
-#     apt_number = db.Column(db.String(50))
-#     city = db.Column(db.String(50))
-#     country = db.Column(db.String(50))
-#     phone_number = db.Column(db.String(50))
-#     salary = db.Column(db.String(50))
-#
-#
-#
-#     def __init__(self, emp_sin_number, first_name, middle_name, last_name, house_number, street_name, postal_code, apt_number, city, country, phone_number, salary):
-#         self.emp_sin_number = emp_sin_number
-#         self.first_name = first_name
-#         self.middle_name = middle_name
-#         self.last_name = last_name
-#         self.house_number = house_number
-#         self.street_name = street_name
-#         self.postal_code = postal_code
-#         self.apt_number = apt_number
-#         self.city = city
-#         self.country = country
-#         self.phone_number = postal_code
-#         self.salary = salary
+class Booking(Base):
+    __tablename__ = 'booking'
+    __table_args__ = {'schema': 'hoteldbs', 'extend_existing': True}
+    booking_number = db.Column(db.Integer, primary_key=True)
+    room_type = db.Column(db.VARCHAR(50))
+    number_occupants = db.Column(db.Integer)
+    renting = db.Column(db.BOOLEAN)
+    check_in_date = db.Column(db.DateTime)
+    check_out_date = db.Column(db.DateTime)
+    total_price = db.Column(db.NUMERIC)
 
 
-# Handling requests that are applicible throughout the entirety of the web app
+class Employee(Base):
+    __tablename__ = 'employee'
+    __table_args__ = {'schema': 'hoteldbs', 'extend_existing': True}
+    cust_sin_number = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.VARCHAR(50))
+    middle_name = db.Column(db.VARCHAR(50))
+    last_name = db.Column(db.VARCHAR(50))
+    house_number = db.Column(db.VARCHAR(50))
+    street_name = db.Column(db.VARCHAR(50))
+    postal_code = db.Column(db.VARCHAR(50))
+    apt_number = db.Column(db.VARCHAR(50))
+    city = db.Column(db.VARCHAR(50))
+    country = db.Column(db.VARCHAR(50))
+    phone_number = db.Column(db.VARCHAR(50))
+    salary = db.Column(db.NUMERIC)
+
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    __table_args__ = {'schema': 'hoteldbs', 'extend_existing': True}
+    cust_sin_number = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.VARCHAR(50))
+    middle_name = db.Column(db.VARCHAR(50))
+    last_name = db.Column(db.VARCHAR(50))
+    house_number = db.Column(db.VARCHAR(50))
+    street_name = db.Column(db.VARCHAR(50))
+    postal_code = db.Column(db.VARCHAR(50))
+    apt_number = db.Column(db.VARCHAR(50))
+    city = db.Column(db.VARCHAR(50))
+    country = db.Column(db.VARCHAR(50))
+    province = db.Column(db.VARCHAR(50))
+    phone_number = db.Column(db.VARCHAR(50))
+    registration_date = db.Column(db.DateTime)
+
+
+# Handling requests that are applicable throughout the entirety of the web app
 @app.route('/backHome', methods=['POST'])
 def backHome():
     if request.method == 'POST':
@@ -109,12 +111,10 @@ def homePage():
     return render_template('homePage.html')
 
 
-
-
 @app.route('/homeSubmit', methods=['POST'])
 def submit():
     result = db.session.query(hotel_chain.c.city)
-    
+
     itemsList = set()
     for r in result:
         itemsList.add(r[0])
@@ -149,14 +149,14 @@ def customerToSearch():
         return render_template('customerSearchPage.html')
 
 
-
 # CUSTOMER SEARCH ROOMS SHOULD BE HERE
+roomDetails = []
+
+
 @app.route('/customerSearchRooms', methods=['POST'])
 def customerSearchRooms():
-
-
     cityList = db.session.query(hotel_chain.c.city)
-    
+
     itemsList = set()
     for city in cityList:
         itemsList.add(city[0])
@@ -165,7 +165,8 @@ def customerSearchRooms():
 
     if request.method == 'POST':
         cityName = request.form['citySelect']
-        bookingDate = request.form['startDate']
+        bookingStartDate = request.form['startDate']
+        bookingEndDate = request.form['endDate']
 
         if cityName == 'Los angeles':
             cityName = 'Los Angeles'
@@ -175,36 +176,48 @@ def customerSearchRooms():
             cityName = 'New York City'
         elif cityName == 'San jose':
             cityName = 'San Jose'
-        
+
         cityName = cityName.capitalize()
-    
-        if cityName == '' or bookingDate == '' or cityName=='holder':
-            return render_template('customerSearchPage.html', message = 'Either cityName or bookingDate is null.', items = itemsList )
+
+        if cityName == '' or bookingStartDate == '' or cityName == 'holder':
+            return render_template('customerSearchPage.html', message='Either cityName or bookingDate is null.',
+                                   items=itemsList)
         else:
-            
-            year = int(bookingDate[0:4])
-            month = int(bookingDate[5:7])
-            day = int(bookingDate[8:])
-            dateTimeFormatted = datetime.datetime(year, month ,day, 0, 0 )
-            result = db.session.query(rooms_hotel_chain,rooms,rooms,hotel_chain).select_from(rooms).join(rooms_hotel_chain).join(hotel_chain).filter(
-                hotel_chain.c.city == cityName and datetime(rooms.c.date_available) <= dateTimeFormatted).order_by(rooms.c.room_number.asc())
-            finalResult = []   
+
+            yearStart = int(bookingStartDate[0:4])
+            monthStart = int(bookingStartDate[5:7])
+            dayStart = int(bookingStartDate[8:])
+
+            yearEnd = int(bookingEndDate[0:4])
+            monthEnd = int(bookingEndDate[5:7])
+            dayEnd = int(bookingEndDate[8:])
+
+            dateTimeFormattedStart = datetime.datetime(yearStart, monthStart, dayStart, 0, 0)
+            dateTimeFormattedEnd = datetime.datetime(yearEnd, monthEnd, dayEnd, 0, 0)
+            roomDetails.append(dateTimeFormattedStart)
+            roomDetails.append(dateTimeFormattedEnd)
+            result = db.session.query(rooms_hotel_chain, rooms, rooms, hotel_chain).select_from(rooms).join(
+                rooms_hotel_chain).join(hotel_chain).filter(
+                hotel_chain.c.city == cityName and datetime(rooms.c.date_available) <= dateTimeFormattedStart).order_by(
+                rooms.c.room_number.asc())
+            finalResult = []
             for r in result:
-                tmpList = [] 
+                tmpList = []
                 x = r[4]
-                if x <= dateTimeFormatted:
+                if x <= dateTimeFormattedStart:
                     tmpR3 = str(r[3])
                     tmpR6 = str(r[6])
                     tmpR7 = str(r[7])
                     tmpR0 = str(r[0])
-
-                    tmpList.append('Price: '+tmpR3)
-                    tmpList.append('View Type: '+ tmpR6)
-                    tmpList.append('Amenities: '+ tmpR7) 
-                    tmpList.append('Room Number: '+ tmpR0)
+                    roomDetails.append(tmpR3)
+                    roomDetails.append(tmpR0)
+                    tmpList.append('Price: ' + tmpR3)
+                    tmpList.append('View Type: ' + tmpR6)
+                    tmpList.append('Amenities: ' + tmpR7)
+                    tmpList.append('Room Number: ' + tmpR0)
                     finalResult.append(tmpList)
                     print(r)
-            return render_template('customerSearchPage.html',items = itemsList, results = result, roomDetails = finalResult)
+            return render_template('customerSearchPage.html', items=itemsList, results=result, roomDetails=finalResult)
 
 
 @app.route('/customerFinishBooking', methods=['POST'])
@@ -218,20 +231,50 @@ def customerFinishBooking():
         cusHouseNum = request.form['customerHouseNumber']
         cusStreetName = request.form['customerStreetName']
         cusPostalCode = request.form['customerPostalCode']
-        cusAptNum = request.form['customerApartmentNumber']        
+        cusAptNum = request.form['customerApartmentNumber']
         cusCityName = request.form['customerCity']
         cusCountryName = request.form['customerCountry']
         cusProvinceName = request.form['customerProvince']
-        cusPhoneNumber = request.form['customerPhoneNumber'] 
+        cusPhoneNumber = request.form['customerPhoneNumber']
+        numOccupants = request.form['numberOfOccupants']
 
-        if cusSINNum == '' or cusFirstName == '' or cusLastName == '' or cusHouseNum == '' or cusStreetName == '' or cusCityName == '' or cusCountryName == '' or cusPostalCode == '' or cusPhoneNumber == '': 
-            return render_template('customerBookingPage.html', message = 'One or more of your required fields are empty, please try again')
+        if cusSINNum == '' or cusFirstName == '' or cusLastName == '' or cusHouseNum == '' or cusStreetName == '' \
+                or cusCityName == '' or cusCountryName == '' or cusPostalCode == '' or cusPhoneNumber == '' \
+                or numOccupants == '':
+            return render_template('customerBookingPage.html',
+                                   message='One or more of your required fields are empty, please try again')
+
+        startDate = roomDetails[0]
+        endDate = roomDetails[1]
+        x = endDate - startDate
+        x = int(x.days)
+        print(x)
+
+        price = float(roomDetails[2])
+        roomNum = roomDetails[3]
+
+        price = price * x
+        roomTypes = ['Single', 'Double', 'Double-double', 'Triple', 'Quad', 'Queen', 'King', 'Studio', 'Twin']
+        roomType = random.choice(roomTypes)
+        bookingNum = random.randint(40, 9999)
+
+        newBooking = Booking(booking_number=bookingNum, room_type=roomType, number_occupants=numOccupants,
+                             renting=False, check_in_date=startDate, check_out_date=endDate,
+                             total_price=price
+                             )
+        db.session.add(newBooking)
+        db.session.commit()
+
+        newCustomer = Customer(cust_sin_number=cusSINNum, first_name=cusFirstName, middle_name=cusMiddleName,
+                               last_name=cusLastName, house_number=cusHouseNum, street_name=cusStreetName,
+                               postal_code=cusPostalCode, apt_number=cusAptNum, city=cusCityName,
+                               country=cusCountryName, province=cusProvinceName, phone_number=cusPhoneNumber,
+                               registration_date=datetime.datetime.today()
+                               )
+        db.session.add(newCustomer)
+        db.session.commit()
 
         return render_template('customerBookingPage.html')
-
-
-    
-
 
 
 # Handling requests on the employee side of the web app
